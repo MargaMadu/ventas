7be2768e3bdb64260be4affc4f1c8d11a5d97ccb
MargaMadu <margacano33@gmail.com>
2025-04-12 22:26:10 +0200

    cambio a excel

--- app.py
+++ app.py
@@ -105,3 +105,4 @@ except FileNotFoundError:
     st.error("❌ No se encontró el archivo 'datos.xlsx'")
 except Exception as e:
     st.error(f"❌ Error al procesar el archivo: {e}")
+    st.download_button("Download as CSV", df.to_csv(), mime="text/csv")
